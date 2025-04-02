1d9adc4cdbadfb01178812fd64d3eba650fbb767
Robbielol <laffanhome@gmail.com>
2025-04-01 21:50:11 -0700

    Adding Sitemap

---
+++ src/app/sitemap.xml/route.js
@@ -0,0 +1,22 @@
+
+export async function GET() {
+    const baseURL = 'https://www.stonehandsmoving.com'
+    const basicRoutes = ['', 'about', 'services', 'reviews', 'contact']
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${staticRoutes.map((route) => `
+            <url>
+            <loc>${baseUrl}/${route}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>${route === "" ? 1.0 : 0.8}</priority>
+            </url>
+        `).join("")}
+        </urlset>
+    `;
+
+    return new Response(sitemap, {
+        headers: {"Content-Type": "application/xml"},
+    });
+}
