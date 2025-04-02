eeb0601676ea4a9f9b3d010304637dcd1f24e6a2
Robbielol <laffanhome@gmail.com>
2025-04-02 14:23:02 -0700

    adding route.js

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,11 +1,11 @@
 
 export async function GET() {
-    const baseURL = 'https://www.stonehandsmoving.com'
+    const baseUrl = 'https://www.stonehandsmoving.com'
     const basicRoutes = ['', 'about', 'services', 'reviews', 'contact']
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${staticRoutes.map((route) => `
+        ${basicRoutes.map((route) => `
             <url>
             <loc>${baseUrl}/${route}</loc>
             <lastmod>${new Date().toISOString()}</lastmod>
